22744cf6c1bc2dc66fdee71f141970c337c2c33a
Ragujan Rag JN <stiflerwedontgiveup@gmail.com>
2024-06-17 18:43:35 +0530

    feat: Owner model and repository created

--- src/main/java/com/rag/pet_clinic_002/PetClinic002/owner/OwnerRepository.java
+++ src/main/java/com/rag/pet_clinic_002/PetClinic002/owner/OwnerRepository.java
@@ -1,5 +1,5 @@
 package com.rag.pet_clinic_002.PetClinic002.owner;
 import org.springframework.data.repository.Repository;
 public interface OwnerRepository  extends Repository{
-  
+  void save(Owner owner);
 }
